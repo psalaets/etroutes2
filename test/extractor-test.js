ba27cbf8da7f506a8c9e86c3793e1c42fa3a351a
Paul Salaets <psalaets@gmail.com>
2013-08-04 22:10:03 -0400

    handle a few special cases

--- test/extractor-test.js
+++ test/extractor-test.js
@@ -1,36 +1,80 @@
 var extractor = require('../lib/extractor');
 var assert = require('assert');
 
-var item = {
-  title: "Donkey Kong - 5.7 - Set by Joe Johnson (Dickey) at Earth Treks (Columbia)",
-  link: 'https://secure.theSendSpot.com/vc/route?rid=5351'
-};
+describe('extractor function', function() {
+  var item = {
+    title: "Donkey Kong - 5.7 - Set by Joe Johnson (Dickey) at Earth Treks (Columbia)",
+    link: 'https://secure.theSendSpot.com/vc/route?rid=5351'
+  };
 
-var itemWithSpaceInGrade = {
-  title: "Donkey Kong - V Intro - Set by Joe Johnson (Dickey) at Earth Treks (Columbia)",
-  link: 'https://secure.theSendSpot.com/vc/route?rid=5351'
-};
-
-var itemWithNoSetterNick = {
-  title: "Donkey Kong - 5.7 - Set by Joe Johnson  at Earth Treks (Columbia)",
-  link: 'https://secure.theSendSpot.com/vc/route?rid=5351'
-};
-
-var itemWithDashesInRouteName = {
-  title: "Donkey -vs- Kong - 5.7 - Set by Joe Johnson (Dickey) at Earth Treks (Columbia)",
-  link: 'https://secure.theSendSpot.com/vc/route?rid=5351'
-};
-
-var itemWithNoGym = {
-  title: "Donkey Kong - V Intro - Set by Joe Johnson (Dickey) at  (Columbia)",
-  link: 'https://secure.theSendSpot.com/vc/route?rid=5351'
-};
-
-describe('extractor', function() {
   it('extracts route from regular item', function() {
     var route = extractor(item.title, item.link);
 
     assert.equal(route.name, 'Donkey Kong');
+    assert.equal(route.grade, '5.7');
+    assert.equal(route.setter, 'Dickey');
+    assert.equal(route.location, 'Columbia');
+    assert.equal(route.url, 'https://secure.theSendSpot.com/vc/route?rid=5351');
+    assert.equal(route.sendSpotId, '5351');
+  });
+
+  var itemWithSpaceInGrade = {
+    title: "Donkey Kong - V Intro - Set by Joe Johnson (Dickey) at Earth Treks (Columbia)",
+    link: 'https://secure.theSendSpot.com/vc/route?rid=5351'
+  };
+
+  it('extracts route from item with space in grade', function() {
+    var route = extractor(itemWithSpaceInGrade.title, itemWithSpaceInGrade.link);
+
+    assert.equal(route.name, 'Donkey Kong');
+    assert.equal(route.grade, 'V Intro');
+    assert.equal(route.setter, 'Dickey');
+    assert.equal(route.location, 'Columbia');
+    assert.equal(route.url, 'https://secure.theSendSpot.com/vc/route?rid=5351');
+    assert.equal(route.sendSpotId, '5351');
+  });
+
+  var itemWithNoSetterNick = {
+    title: "Donkey Kong - 5.7 - Set by Joe Johnson  at Earth Treks (Columbia)",
+    link: 'https://secure.theSendSpot.com/vc/route?rid=5351'
+  };
+
+  it('uses setter name when theres no setter nick', function() {
+    var route = extractor(itemWithNoSetterNick.title, itemWithNoSetterNick.link);
+
+    assert.equal(route.name, 'Donkey Kong');
+    assert.equal(route.grade, '5.7');
+    assert.equal(route.setter, 'Joe Johnson');
+    assert.equal(route.location, 'Columbia');
+    assert.equal(route.url, 'https://secure.theSendSpot.com/vc/route?rid=5351');
+    assert.equal(route.sendSpotId, '5351');
+  });
+
+  var itemWithNoGym = {
+    title: "Donkey Kong - 5.7 - Set by Joe Johnson (Dickey) at  (Columbia)",
+    link: 'https://secure.theSendSpot.com/vc/route?rid=5351'
+  };
+
+  it('extracts route when item has no gym', function() {
+    var route = extractor(itemWithNoGym.title, itemWithNoGym.link);
+
+    assert.equal(route.name, 'Donkey Kong');
+    assert.equal(route.grade, '5.7');
+    assert.equal(route.setter, 'Dickey');
+    assert.equal(route.location, 'Columbia');
+    assert.equal(route.url, 'https://secure.theSendSpot.com/vc/route?rid=5351');
+    assert.equal(route.sendSpotId, '5351');
+  });
+
+  var itemWithDashesInRouteName = {
+    title: "Donkey -vs- Kong - 5.7 - Set by Joe Johnson (Dickey) at Earth Treks (Columbia)",
+    link: 'https://secure.theSendSpot.com/vc/route?rid=5351'
+  };
+
+  it('extracts route when route name has dashes in it', function() {
+    var route = extractor(itemWithDashesInRouteName.title, itemWithDashesInRouteName.link);
+
+    assert.equal(route.name, 'Donkey -vs- Kong');
     assert.equal(route.grade, '5.7');
     assert.equal(route.setter, 'Dickey');
     assert.equal(route.location, 'Columbia');
