71750221378887a28b9bf3b601ee3d79bd5af90e
Paul Salaets <psalaets@gmail.com>
2013-10-13 12:43:13 -0400

    show real routes

--- routes/route.js
+++ routes/route.js
@@ -4,31 +4,10 @@ var routeTweeter = require('../lib/routeTweeter');
 var Route = mongoose.model('Route');
 
 exports.feed = function(req, res) {
-  Route.find(function(err, docs) {
+  Route.find().sort({sendspotId: -1}).exec(function(err, docs) {
     res.render('feed', {
       tweet: routeTweeter,
-      routes: [{
-        name: 'The Climb',
-        grade: '5.10a',
-        setter: 'Bob',
-        location: 'Rockville',
-        url: 'http://something.com',
-        sendspotId: 5555
-      }, {
-        name: 'The Fall',
-        grade: '5.12a',
-        setter: 'Bob',
-        location: 'Rockville',
-        url: 'http://something.com',
-        sendspotId: 5550
-      }, {
-        name: 'The Jump',
-        grade: '5.11a',
-        setter: 'Bob',
-        location: 'Rockville',
-        url: 'http://something.com',
-        sendspotId: 5552
-      }]
+      routes: docs
     });
   });
 };
