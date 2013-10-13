15005b06ca6f0439ed9370265c4475875cf7c35e
Paul Salaets <psalaets@gmail.com>
2013-10-13 12:20:59 -0400

    start polling job

--- app.js
+++ app.js
@@ -42,3 +42,5 @@ app.get('/routes/latest', route.feed);
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+require('./lib/poller').start();
