7aab10f8581596d224e415165d76f9833e96f7e7
Paul Salaets <psalaets@gmail.com>
2013-10-13 15:10:01 -0400

    move scheduling to its own module

--- app.js
+++ app.js
@@ -43,4 +43,4 @@ http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
 
-require('./lib/poller').start();
+require('./lib/scheduler').start();
