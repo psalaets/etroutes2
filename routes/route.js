b02addce2f4ec5b4dcca6581db6001574fe8c614
Paul Salaets <psalaets@gmail.com>
2013-10-19 21:56:17 -0400

    set outgoing content-type

--- routes/route.js
+++ routes/route.js
@@ -5,6 +5,7 @@ var Route = mongoose.model('Route');
 
 exports.feed = function(req, res) {
   Route.find().sort({sendspotId: -1}).exec(function(err, docs) {
+    res.setHeader('Content-Type', 'application/rss+xml');
     res.render('feed', {
       tweet: routeTweeter,
       routes: docs
