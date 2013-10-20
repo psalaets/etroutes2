724303ab74704e450599f44c2dd1bd9dbf8c3aed
Paul Salaets <psalaets@gmail.com>
2013-10-19 21:17:05 -0400

    make production mongo url from vcap env var

--- config.js
+++ config.js
@@ -1,8 +1,24 @@
+var productionMongo;
+
+if (process.env.VCAP_SERVICES) {
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  productionMongo = buildMongoUrl(env['mongodb-1.8'][0]['credentials']);
+}
+
+function buildMongoUrl(obj) {
+  if (obj.username && obj.password) {
+    return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  } else {
+    return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  }
+}
+
+
 module.exports = {
   development: {
     db: 'mongodb://localhost/etroutes-dev'
   },
   production: {
-    db: process.env.MONGODB_URL
+    db: productionMongo
   }
 };
